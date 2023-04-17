47b62c3a57f20b2f5171b0aba40269339d3b34bd
Artstiom Ezepchik <artem.ezepchik@gmail.com>
2023-04-17 16:12:14 +0300

    minor refactor

--- src/helpers/requests/getWeatherByCityName/getWeatherByCityName.ts
+++ src/helpers/requests/getWeatherByCityName/getWeatherByCityName.ts
@@ -1,7 +1,7 @@
-import { transformWeatherPayload } from "../../transformWeatherPayload/transformWeatherPayload";
+import { transformWeatherPayload } from "../../transformWeatherPayload/transformWeatherPayload"
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/';
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = 'https://api.openweathermap.org/data/2.5/'
+const API_KEY = process.env.REACT_APP_API_KEY
 
 export const getWeatherByCityName = async (city: string) => {
   const response = await fetch(`${API_URL}forecast?q=${city}&units=metric&appid=${API_KEY}`)
